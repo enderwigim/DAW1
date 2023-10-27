8a52bc98bb90de985ec4d677289906d703518265
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2023-10-27 20:06:39 +0200

    Entornos

---
+++ Entornos/Tema 3/Ejercicio 10/Ejercicio 10/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Ejercicio_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbUrgente = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtTelegrama = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(106, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TEXTO:";
+            // 
+            // cbUrgente
+            // 
+            this.cbUrgente.AutoSize = true;
+            this.cbUrgente.Location = new System.Drawing.Point(110, 318);
+            this.cbUrgente.Name = "cbUrgente";
+            this.cbUrgente.Size = new System.Drawing.Size(70, 17);
+            this.cbUrgente.TabIndex = 2;
+            this.cbUrgente.Text = "Urgente?";
+            this.cbUrgente.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(595, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 51);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "COSTE:";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(162, 370);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(118, 20);
+            this.txtPrecio.TabIndex = 5;
+            // 
+            // txtTelegrama
+            // 
+            this.txtTelegrama.Location = new System.Drawing.Point(110, 83);
+            this.txtTelegrama.Name = "txtTelegrama";
+            this.txtTelegrama.Size = new System.Drawing.Size(539, 212);
+            this.txtTelegrama.TabIndex = 1;
+            this.txtTelegrama.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbUrgente);
+            this.Controls.Add(this.txtTelegrama);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cbUrgente;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.RichTextBox txtTelegrama;
+    }
+}
+
---
+++ Entornos/Tema 3/Ejercicio 10/Ejercicio 10/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string textoTelegrama;
+            char tipoTelegrama = ' ';
+            int numPalabras = 0;
+            double coste;
+
+            //Leo el telegrama
+            textoTelegrama = txtTelegrama.Text;
+
+            // telegrama urgente?
+            if (cbUrgente.Checked)
+            {
+                tipoTelegrama = 'u';
+            }
+            else
+            {
+                tipoTelegrama = 'o';
+            }
+            //Obtengo el número de palabras que forma el telegrama 
+            numPalabras = (textoTelegrama.Split()).Lenght;
+            //Si el telegrama es ordinario
+            if (tipoTelegrama == 'o')
+                if (numPalabras <= 10)
+                    coste = 2.5;
+                else
+                {
+                    coste = 0.5 * numPalabras;
+                }
+            else
+            {
+                //Si el telegrama es urgente
+                if (tipoTelegrama == 'u')
+                    if (numPalabras <= 10)
+                        coste = 5;
+                    else
+                    {
+                        coste = 5 + 0.75 * (numPalabras - 10);
+                    }
+                else
+                    coste = 0;
+            }
+            txtPrecio.Text = coste.ToString() + " euros";
+        }
+    }
+}

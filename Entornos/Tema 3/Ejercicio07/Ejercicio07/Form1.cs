14f50e6e24c037881089a02ac3b587d8307a2be8
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2023-11-08 16:55:53 +0100

    Fieldset and Legend added to the form.

---
+++ Entornos/Tema 3/Ejercicio07/Ejercicio07/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int maxDiv(int n1, int n2)
+        {
+            int divisorN1;
+            divisorN1 = 0;
+            for (int i = 1; i <= n1 && i <= n2; i++)
+            {
+                if (n1 % i == 0 && n2 % i == 0)
+                {
+                    divisorN1 = i;
+                }
+            }
+    
+
+            return divisorN1;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1, num2, maximoComunDiv;
+
+            num1 = int.Parse(txtNum.Text);
+            num2 = int.Parse(txtNum2.Text);
+            
+            maximoComunDiv = maxDiv(num1, num2);
+
+            MessageBox.Show(maximoComunDiv.ToString());
+        }
+    }
+}

c73cfc05980cbc18de5a067786a97faaf8a6eefd
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2023-11-15 20:19:10 +0100

    Ordenar Numeros Creado

--- PROGRAMACION/Ejercicio02/Ejercicio02/Program.cs
+++ PROGRAMACION/Ejercicio02/Ejercicio02/Program.cs
@@ -10,6 +10,38 @@ namespace Ejercicio02
     {
         static void Main(string[] args)
         {
+            float media;
+            int[] numero = new int[4];
+            int[] numerosOrden = new int[4];
+            numero[0] = 100; 
+            numero[1] = 50;
+            numero[2] = 200;
+            numero[3] = 150;
+
+            for (int i = 0; i < numero.Length; i++)
+            {
+                for (int j = 0; j < numero.Length; j++)
+                {
+                    if (numero[i] < numero[j])
+                    {
+                        numerosOrden[].(numerosOrden[j]);
+                    }
+                }
+            }
+            Console.ReadLine();
+
+            /* while (i < 4)
+            {
+                Console.WriteLine("Introduce un numero");
+                numero[i] = int.Parse(Console.ReadLine());
+                i++;
+            }
+            */
+
+
+
+
+
         }
     }
 }

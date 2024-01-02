32235c893329c37c2fc582c97c0884e77068d588
Santiago Lorenzano <enderwigim@gmail.com>
2024-01-02 19:50:20 +0100

    Ejercicio 07 finished

---
+++ Entornos/Tema 4/Ejercicio07/Ejercicio07/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 2;
+            int s = 0;
+            int n;
+            n = Int32.Parse(Console.ReadLine());
+            while (j <= n / 2)
+            {
+                if (n % j == 0)
+                {
+                    s = s + 1;
+
+                }
+                j = j + 1;
+            }
+                
+
+            if (s == 0)
+                Console.Write(n + "es primo");
+            else
+                Console.Write(n + "no es primo");
+        }
+    }
+}
+

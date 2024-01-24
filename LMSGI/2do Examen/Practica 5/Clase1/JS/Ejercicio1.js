e51fc0ad4d59eae56ff5696d7a4507dd462bff91
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2024-01-24 19:08:06 +0100

    Clase 1 finished

--- LMSGI/2do Examen/Practica 5/Clase1/JS/Ejercicio1.js
+++ LMSGI/2do Examen/Practica 5/Clase1/JS/Ejercicio1.js
@@ -1,1 +1,95 @@
-alert("Hola Mundo");
+// alert("Hola Mundo");
+
+
+// edad = confirm("Eres mayor de edad?");
+// if (edad) {
+    //     alert("DISFRUTA xD")
+    // } else {
+        //     alert("Te falta leche todavia.")
+        // }
+        
+// let edad;
+// let nombre;
+// edad = prompt("Introduce tu edad");
+// nombre = prompt("Introduce tu nombre");
+
+// if(edad >= 18 && nombre == 'Santiago') {
+//     alert("Eres mayor de edad y eres " + nombre)
+// }
+// else if (edad >= 18 || nombre == 'Santiago') {
+//     if (edad >= 18) {
+//         alert("Eres mayor de edad")
+//     } else {
+//         alert("Eres Santiago")
+//     }
+// }
+// else {
+//     alert("Todavía no eres mayor de edad")
+// };
+
+//while
+// let x=1;
+
+// while(x<= 100) {
+//     document.write(x);
+//     document.write('<br>');
+//     x++;
+// }
+
+//for
+// for(let f=1;f<=100;f++) {
+//     document.write(f + ' ');
+// }
+
+//do while
+// let valor;
+// do {
+//     valor = prompt("Introduce un valor en 0 y 999 (con 0 terminas)")
+//     document.write('El numero ' + valor + ": ")
+//     if (valor < 10) {
+//         document.write('Tiene un digito');
+//     }
+//     else if (valor < 100) {
+//         document.write('Tiene dos digitos');
+//     }
+//     else {
+//         document.write('Tiene tres digitos');
+//     }
+//     document.write('<br>')
+// } while(valor == 0);
+
+//Ejemplo1
+// let number = 1;
+// while (number <= 100) {
+//     if (number % 2 == 0) {
+//         document.write(number + "<br>");
+//     }
+//     number++;
+// }
+
+//Ej2
+// let sum = 0;
+// for(let i=1; i <= 10; i++) {
+//     sum += i;
+// }
+// document.write(sum);
+
+//Ej3
+
+const random_number = Math.floor(Math.random()*10)+1;
+let founded = false;
+
+for(let i = 1; i <= 3 && !founded; i++) {
+    let num = parseInt(prompt("Enter a number"));
+    if (num === random_number) {
+        founded = true;
+        alert("Well done! You win!")
+    }
+    else if (3 - i != 0){
+        alert("You still have " + (3 - i) + " tries");
+    }
+
+}
+if (!founded) {
+    alert("Computer wins!");
+}

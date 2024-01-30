395a30109e5669bf420d882463985b26df197019
Santiago Lorenzano <enderwigim@gmail.com>
2024-01-30 11:19:52 +0100

    IDE Practica 4.3 uploaded

---
+++ IDE/Second Term/Tema 4/Practica 4.3 Unit Test/GestionBancariaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GestionBancariaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("IES Mare Nostrum")]
+[assembly: AssemblyProduct("GestionBancariaTest")]
+[assembly: AssemblyCopyright("Copyright © IES Mare Nostrum 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d76c449-e86f-48c7-8e2a-3ab5edda99f3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

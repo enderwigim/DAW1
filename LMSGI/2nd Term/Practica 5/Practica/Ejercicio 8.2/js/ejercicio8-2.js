58d75666db14776fae7c2800ec1212aca5c2d866
Santiago Lorenzano <enderwigim@gmail.com>
2024-02-11 01:29:36 +0100

    Added new functionality. Text can be lined-through
     if checkbox is clicked text inside the list will be lined-through

--- LMSGI/2nd Term/Practica 5/Practica/Ejercicio 8.2/js/ejercicio8-2.js
+++ LMSGI/2nd Term/Practica 5/Practica/Ejercicio 8.2/js/ejercicio8-2.js
@@ -2,10 +2,14 @@ let ul = document.getElementById("list");
 let add_element_btn = document.getElementById("add_element");
 let delete_last_btn = document.getElementById("delete_last_element");
 
+
 add_element_btn.addEventListener("click", function() {
     let new_text = prompt("Añade un nuevo elemento.");
     let li = document.createElement("li");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
     let li_text = document.createTextNode(new_text);
+    li.appendChild(checkbox);
     li.appendChild(li_text);
     ul.appendChild(li);
 })
@@ -16,3 +20,21 @@ delete_last_btn.addEventListener("click", function() {
 
 })
 
+// With this, we'll create an eventListenert for the father, the UL.
+ul.addEventListener("click", function(e) {
+    // Once ul was clicked, we'll triger a function that recieves e as a parameter.
+    // e is used in JS as an event.
+    // After that, we will use target to select the item clicked, and we will check if it was an input.
+    if(e.target.nodeName == "INPUT") {
+        // We will select the parent, the LI, which has a text added and we will set a new style to it.
+        let li = e.target.parentElement;
+        
+        if (li.style.textDecoration != 'line-through'){
+            li.style.textDecoration = 'line-through';
+
+        } else {
+            li.style.textDecoration = 'none';
+        }
+        
+    }
+})

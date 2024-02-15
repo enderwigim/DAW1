4a76f04416d20c4dd6c8bad0a3bb78664728850c
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2024-02-15 16:45:31 +0100

    Practica6-1 Started

---
+++ IDE/Second Term/Tema 6/Practica 6-1/Fechas/Fechas/Fecha.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fechas
+{
+    internal class Fecha
+    {
+        private int dia;
+        private int mes;
+        private int anyo;
+
+        //TODO validar los valores introducidos
+        /// <summary>
+        /// Constructor de Fecha sin parámetros
+        /// Se establecen los valores a 1
+        /// </summary>
+        public Fecha()
+        {
+            fDia = 1;
+            fMes = 1;
+            fAnyo = 1;
+        }
+
+        public int fDia
+        {
+            get { return dia; }
+            set { dia = value; }
+        }
+        public int fMes
+        {
+            get { return mes; }
+            set { mes = value;}
+        }
+        public int fAnyo
+        {
+            get { return anyo; }
+            set { anyo = value; }
+        }
+
+        /// <summary>
+        /// Constructor de Fecha con 3 parámetros
+        /// Si algún parámetro no es correcto se establece a 1
+        /// </summary>
+        /// <param name="mes">Mes</param>
+        /// <param name="anyo">Anyo (entre 1 y 2500)</param>
+        /// <param name="dia">Dia</param>
+        /// <param name="bi">Indica si es bisiesto</param>
+        public Fecha(int mes, int anyo, int dia, bool bi)
+        {
+            if (anyo >= 1 && anyo <= 2500)
+            {
+                fAnyo = anyo;
+            }
+            else
+            {
+                fAnyo = 1;
+            }
+
+            if (mes >= 1 && mes <= 12)
+                fMes = mes;
+            else
+                fMes = 1;
+
+            int diasMes = CalcDiasMes();
+            if (dia >= 1 && dia <= diasMes)
+                fDia = dia;
+            else
+                fDia = 1;
+        }
+
+        private int CalcDiasMes()
+        {
+            int diasMes = 0;
+
+            if (fMes == 2)
+            {
+                if (this.EsBisiesto())
+                    diasMes = 29;
+                else
+                    diasMes = 28;
+            }
+            else if (fMes == 4 || fMes == 6 || fMes == 9 || fMes == 11)
+            {
+                diasMes = 30;
+            }
+            else
+            {
+                diasMes = 31;
+            }
+            return diasMes;
+        }
+
+        public bool EsBisiesto()
+        {
+            bool bisiesto = false;
+            if ((fAnyo % 400 == 0) || ((fAnyo % 4 == 0) && (fAnyo % 100 != 0)))
+                bisiesto = true;
+            else bisiesto = false;
+            return bisiesto;
+        }
+
+
+        /// <summary>
+        /// Devuelve un string con la fecha en formato dia/mes/anyo
+        /// </summary>
+        /// <remarks> la palabra clave override indica que se  sobreescribe el metodo ToString</remarks>
+        /// <returns>un string con la fecha en formato dia/mes/anyo</returns> public override string
+        override
+        public string ToString()
+        {
+            return fDia + "/" + fMes + "/" + fAnyo;
+        }
+    }
+}
---
+++ IDE/Second Term/Tema 6/Practica 6-1/Fechas/Fechas/Program.cs
@@ -0,0 +1,38 @@
+﻿using Fechas;
+using System;
+
+namespace Fechas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fecha correcta, año bisiesto
+            Fecha fecha1 = new Fecha(12, 2012, 4, false);
+            Console.WriteLine("Fecha 1: " + fecha1.ToString());
+
+            if (fecha1.EsBisiesto())
+                Console.WriteLine("El año " + fecha1.fAnyo + " es bisiesto");
+            else
+                Console.WriteLine("El año " + fecha1.fAnyo + " no es bisiesto");
+
+            //Fecha correcta, año no bisiesto
+            Fecha fecha2 = new Fecha(10, 2013, 4, false);
+            Console.WriteLine("Fecha 2: " + fecha2.ToString());
+            if (fecha2.EsBisiesto())
+                Console.WriteLine("El año " + fecha2.fAnyo + " es bisiesto");
+            else
+                Console.WriteLine("El año " + fecha2.fAnyo + " no es bisiesto");
+
+            //Fecha con valores incorrectos
+            Fecha fecha3 = new Fecha(13, -4, 4, false);
+            Console.WriteLine("Fecha 3: " + fecha3.ToString());
+
+            //Fecha con asignación incorrecta de valores erroneos
+            Fecha fecha4 = new Fecha(); fecha4.fDia = 67;
+            fecha4.fMes = 80;
+            fecha4.fAnyo = 3678;
+            Console.WriteLine("Fecha 4: " + fecha4.ToString());
+        }
+    }
+}

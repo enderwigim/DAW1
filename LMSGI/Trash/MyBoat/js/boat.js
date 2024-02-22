f1bd931c0ac32dbe657707b3925245359af6563e
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2024-02-22 16:33:41 +0100

    Create ListOfBoats constructor

--- LMSGI/Trash/MyBoat/js/boat.js
+++ LMSGI/Trash/MyBoat/js/boat.js
@@ -1,12 +1,23 @@
 let map  = {
     height: 10,
-    weight: 10,
+    width: 10,
+
+    
+
+
+
+}
+
+
+let enemyBoats = {
+    maxSegments: 3,
     enemyBoats: [],
+    ammountOfSegments: [],
 
     createBoats: function() {
         for(let i = 1; i <= 5; i++) {
-            let randomX = this.GetRandomNumber(this.height);
-            let randomY = this.GetRandomNumber(this.weight);
+            let randomX = this.GetRandomNumber(height);
+            let randomY = this.GetRandomNumber(weight);
             this.enemyBoats.push(new Boat(randomX, randomY));
         }
     },
@@ -33,19 +44,64 @@ let map  = {
         return this.enemyBoats.length;
     },
 
-    Check: function() {
-        let everyPosition = "";
-        for (let i = 0; i < this.CountBoats(); i++) {
-            everyPosition += this.enemyBoats[i].GetX() + " " + this.enemyBoats[i].GetY() +  "\n";
-        }
-        return everyPosition;
-    }
 }
 
-function Boat(dx, dy) {
-    this.x = dx;
-    this.y = dy,
+function ListOfBoats(mapHeight, mapWidth) {
+    this.boatList = [];
+    this.ammountOfSegments = [];
+    this.maxSegments = 3;
 
+    this.createBoats = function(ammount) {
+        for(let i = 1; i <= ammount; i++) {
+            let randomX = this.GetRandomNumber(mapHeight);
+            let randomY = this.GetRandomNumber(mapWidth);
+            let segment = this.GetRandomNumber(this.maxSegments);
+            let direction = this.SetDirection(randomX, randomY, segment, mapWidth, mapHeight);
+            this.enemyBoats.push(new Boat(segment, randomX, randomY, direction));
+        }
+    };
+    this.GetRandomNumber = function(max) {
+        return Math.floor(Math.random() * max) + 1;
+    };
+    this.SetDirection = function(x, y, segment, mapWidth, mapHeight) {
+        let direction;
+        if (x - segment <= 0) {
+            direction = "RIGHT";
+        } else if (x - segment >= mapWidth) {
+            direction = "LEFT";
+        } else if (y - segment <= 0) {
+            direction = "UP";
+        } else if (y - segment >= mapHeight) {
+            direction = "DOWN";
+        } else {
+            direction = this.SetRandomDirection();
+        }
+        return direction;
+    };
+    this.SetRandomDirection = function() {
+        let posibility = this.GetRandomNumber(4);
+        switch (posibility) {
+            case 1:
+                return "RIGHT"
+                
+            case 2:
+                return "LEFT"
+            case 3:
+                return "UP"
+            case 4:
+                return "DOWN"
+            default:
+                return "RIGHT"
+        }
+    }
+
+}
+function Boat(segments, dx, dy, direction) {
+    this.segments = segments;
+    this.xPosition = [dx];
+    this.yPosition = [dy];
+    this.direction = direction;
+    /*
     this.GetX = function() {
         return this.x;
     };
@@ -53,6 +109,11 @@ function Boat(dx, dy) {
     this.GetY = function(){
         return this.y;
     }
+    this.AddInitialCoord = function(dx, dy) {
+        this.xPosition.push(dx);
+        this.yPosition.push(dy);
+    }
+    */
 
 }
 

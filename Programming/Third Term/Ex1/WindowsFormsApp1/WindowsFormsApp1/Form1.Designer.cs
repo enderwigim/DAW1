466cdfab230a1dfc18c390b33e8cfc84b5f580e3
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2024-04-15 18:50:21 +0200

    Class 15/04 Finished

--- Programming/Third Term/Ex1/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ Programming/Third Term/Ex1/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,21 +28,288 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnShowLastTeacher = new System.Windows.Forms.Button();
+            this.btnShowNextTeacher = new System.Windows.Forms.Button();
+            this.btnShowPreviousTeacher = new System.Windows.Forms.Button();
+            this.btnShowFirstTeacher = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSaveNew = new System.Windows.Forms.Button();
+            this.btnAddTeacher = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.txteMail = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtTlf = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DNI:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tlf:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(272, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Apellidos:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(290, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "email:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnShowLastTeacher);
+            this.groupBox1.Controls.Add(this.btnShowNextTeacher);
+            this.groupBox1.Controls.Add(this.btnShowPreviousTeacher);
+            this.groupBox1.Controls.Add(this.btnShowFirstTeacher);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(29, 243);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(739, 87);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Navegar";
+            // 
+            // btnShowLastTeacher
+            // 
+            this.btnShowLastTeacher.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.btnShowLastTeacher.Location = new System.Drawing.Point(572, 19);
+            this.btnShowLastTeacher.Name = "btnShowLastTeacher";
+            this.btnShowLastTeacher.Size = new System.Drawing.Size(161, 44);
+            this.btnShowLastTeacher.TabIndex = 3;
+            this.btnShowLastTeacher.Text = "Ultimo";
+            this.btnShowLastTeacher.UseVisualStyleBackColor = true;
+            this.btnShowLastTeacher.Click += new System.EventHandler(this.btnShowLastTeacher_Click);
+            // 
+            // btnShowNextTeacher
+            // 
+            this.btnShowNextTeacher.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.btnShowNextTeacher.Location = new System.Drawing.Point(400, 19);
+            this.btnShowNextTeacher.Name = "btnShowNextTeacher";
+            this.btnShowNextTeacher.Size = new System.Drawing.Size(161, 44);
+            this.btnShowNextTeacher.TabIndex = 2;
+            this.btnShowNextTeacher.Text = "Siguiente";
+            this.btnShowNextTeacher.UseVisualStyleBackColor = true;
+            this.btnShowNextTeacher.Click += new System.EventHandler(this.btnShowNextTeacher_Click);
+            // 
+            // btnShowPreviousTeacher
+            // 
+            this.btnShowPreviousTeacher.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.btnShowPreviousTeacher.Location = new System.Drawing.Point(214, 19);
+            this.btnShowPreviousTeacher.Name = "btnShowPreviousTeacher";
+            this.btnShowPreviousTeacher.Size = new System.Drawing.Size(161, 44);
+            this.btnShowPreviousTeacher.TabIndex = 1;
+            this.btnShowPreviousTeacher.Text = "Anterior";
+            this.btnShowPreviousTeacher.UseVisualStyleBackColor = true;
+            this.btnShowPreviousTeacher.Click += new System.EventHandler(this.btnShowPreviousTeacher_Click);
+            // 
+            // btnShowFirstTeacher
+            // 
+            this.btnShowFirstTeacher.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.btnShowFirstTeacher.Location = new System.Drawing.Point(18, 19);
+            this.btnShowFirstTeacher.Name = "btnShowFirstTeacher";
+            this.btnShowFirstTeacher.Size = new System.Drawing.Size(161, 44);
+            this.btnShowFirstTeacher.TabIndex = 0;
+            this.btnShowFirstTeacher.Text = "Primero";
+            this.btnShowFirstTeacher.UseVisualStyleBackColor = true;
+            this.btnShowFirstTeacher.Click += new System.EventHandler(this.btnShowFirstTeacher_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSaveNew);
+            this.groupBox2.Controls.Add(this.btnAddTeacher);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox2.Location = new System.Drawing.Point(29, 351);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(375, 87);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Nuevo Registro";
+            // 
+            // btnSaveNew
+            // 
+            this.btnSaveNew.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.btnSaveNew.Location = new System.Drawing.Point(185, 19);
+            this.btnSaveNew.Name = "btnSaveNew";
+            this.btnSaveNew.Size = new System.Drawing.Size(161, 44);
+            this.btnSaveNew.TabIndex = 5;
+            this.btnSaveNew.Text = "Guardar Nuevo";
+            this.btnSaveNew.UseVisualStyleBackColor = true;
+            this.btnSaveNew.Click += new System.EventHandler(this.btnSaveNew_Click);
+            // 
+            // btnAddTeacher
+            // 
+            this.btnAddTeacher.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.btnAddTeacher.Location = new System.Drawing.Point(18, 19);
+            this.btnAddTeacher.Name = "btnAddTeacher";
+            this.btnAddTeacher.Size = new System.Drawing.Size(161, 44);
+            this.btnAddTeacher.TabIndex = 4;
+            this.btnAddTeacher.Text = "Añadir";
+            this.btnAddTeacher.UseVisualStyleBackColor = true;
+            this.btnAddTeacher.Click += new System.EventHandler(this.btnAddTeacher_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnDelete);
+            this.groupBox3.Controls.Add(this.btnUpdate);
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox3.Location = new System.Drawing.Point(429, 351);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(339, 87);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Actualizar y Eliminar";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.btnDelete.Location = new System.Drawing.Point(172, 19);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(161, 44);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Eliminar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.btnUpdate.Location = new System.Drawing.Point(6, 19);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(161, 44);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Actualizar";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Location = new System.Drawing.Point(79, 39);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(170, 20);
+            this.txtDNI.TabIndex = 8;
+            // 
+            // txteMail
+            // 
+            this.txteMail.Location = new System.Drawing.Point(330, 186);
+            this.txteMail.Name = "txteMail";
+            this.txteMail.Size = new System.Drawing.Size(170, 20);
+            this.txteMail.TabIndex = 9;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(330, 122);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(170, 20);
+            this.txtApellidos.TabIndex = 10;
+            // 
+            // txtTlf
+            // 
+            this.txtTlf.Location = new System.Drawing.Point(79, 186);
+            this.txtTlf.Name = "txtTlf";
+            this.txtTlf.Size = new System.Drawing.Size(170, 20);
+            this.txtTlf.TabIndex = 11;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(79, 122);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(170, 20);
+            this.txtNombre.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtTlf);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txteMail);
+            this.Controls.Add(this.txtDNI);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Profesores";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnShowLastTeacher;
+        private System.Windows.Forms.Button btnShowNextTeacher;
+        private System.Windows.Forms.Button btnShowPreviousTeacher;
+        private System.Windows.Forms.Button btnShowFirstTeacher;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSaveNew;
+        private System.Windows.Forms.Button btnAddTeacher;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.TextBox txteMail;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtTlf;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
     }
 }
 
--- Programming/Third Term/Ex1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ Programming/Third Term/Ex1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -19,6 +19,8 @@ namespace WindowsFormsApp1
         }
         DataSet dataSetProfs;
         SqlDataAdapter dataAdapterProfs;
+        private int pos;
+        private int maxRegistros;
         private void Form1_Load(object sender, EventArgs e)
         {
             string cadenaConexion = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\sanlor2\\Desktop\\Repos\\1Daw\\1daw\\Programming\\Third Term\\Ex1\\WindowsFormsApp1\\WindowsFormsApp1\\App_Data\\Instituto.mdf;Integrated Security=True";
@@ -27,17 +29,102 @@ namespace WindowsFormsApp1
             con.Open();
 
             string cadenaSQL = "SELECT * From Profesores";
-
             dataAdapterProfs = new SqlDataAdapter(cadenaSQL, con);
-
             dataSetProfs = new DataSet();
-
             dataAdapterProfs.Fill(dataSetProfs, "Profesores");
 
-
+            pos = 0;
+            maxRegistros = dataSetProfs.Tables["Profesores"].Rows.Count;
+            MostrarRegistro(pos);
 
             // Cerramos la conexion
             con.Close();
+        }
+        public void MostrarRegistro(int pos)
+        {
+            // Objeto que nos permite recoger un registro de la tabla.
+            DataRow dRegistro;
+            // Cogemos el registro de la posición pos en la tabla Profesores
+            dRegistro = dataSetProfs.Tables["Profesores"].Rows[pos];
+            // Cogemos el valor de cada una de las columnas del registro
+            // y lo ponemos en el textBox correspondiente.
+
+            txtDNI.Text = dRegistro[0].ToString();
+            txtNombre.Text = dRegistro[1].ToString();
+            txtApellidos.Text = dRegistro[2].ToString();
+            txtTlf.Text = dRegistro[3].ToString();
+            txteMail.Text = dRegistro[4].ToString();
+
+        }
+
+        private void btnShowFirstTeacher_Click(object sender, EventArgs e)
+        {
+            
+            pos = 0;
+            MostrarRegistro(pos);
+        }
+
+        private void btnShowNextTeacher_Click(object sender, EventArgs e)
+        {
+            if (pos != maxRegistros - 1)
+            {
+                pos++;
+            }
+            else
+            {
+                pos = 0;
+            }
+            MostrarRegistro(pos);
+        }
+
+        private void btnShowPreviousTeacher_Click(object sender, EventArgs e)
+        {
+            if (pos != 0)
+            {
+                pos--;
+            }
+            else
+            {
+                pos = maxRegistros - 1;
+            }
+            MostrarRegistro(pos);
+            
+        }
+
+        private void btnShowLastTeacher_Click(object sender, EventArgs e)
+        {
+            pos = maxRegistros - 1;
+            MostrarRegistro(pos);
+        }
+
+        private void btnAddTeacher_Click(object sender, EventArgs e)
+        {
+            txtDNI.Clear();
+            txtNombre.Clear();
+            txtApellidos.Clear();
+            txtTlf.Clear();
+            txteMail.Clear();
+        }
+
+        private void btnSaveNew_Click(object sender, EventArgs e)
+        {
+            // Creamos un nuevo registro.
+            DataRow dRegistro = dataSetProfs.Tables["Profesores"].NewRow();
+            // Metemos los datos en el nuevo registro
+            dRegistro[0] = txtDNI.Text;
+            dRegistro[1] = txtNombre.Text;
+            dRegistro[2] = txtApellidos.Text;
+            dRegistro[3] = txtTlf.Text;
+            dRegistro[4] = txteMail.Text;
+
+            // Añadimos el registro al Dataset
+            dataSetProfs.Tables["Profesores"].Rows.Add(dRegistro);
+            // Reconectamos con el dataAdapter y actualizamos la BD
+            SqlCommandBuilder cb = new SqlCommandBuilder(dataAdapterProfs);
+            dataAdapterProfs.Update(dataSetProfs, "Profesores");
+            // Actualizamos el número de registros y la posición en la tabla
+            maxRegistros++;
+            pos = maxRegistros - 1;
         }
     }
 }

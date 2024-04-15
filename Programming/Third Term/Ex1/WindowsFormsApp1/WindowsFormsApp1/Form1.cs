9d61e6f1a2b7964397d5702386892db7f4c904b0
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2024-04-15 18:12:01 +0200

    Form created

--- Programming/Third Term/Ex1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ Programming/Third Term/Ex1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +16,28 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+        DataSet dataSetProfs;
+        SqlDataAdapter dataAdapterProfs;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cadenaConexion = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\sanlor2\\Desktop\\Repos\\1Daw\\1daw\\Programming\\Third Term\\Ex1\\WindowsFormsApp1\\WindowsFormsApp1\\App_Data\\Instituto.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(cadenaConexion);
+            // Abrimos la conexión.
+            con.Open();
+
+            string cadenaSQL = "SELECT * From Profesores";
+
+            dataAdapterProfs = new SqlDataAdapter(cadenaSQL, con);
+
+            dataSetProfs = new DataSet();
+
+            dataAdapterProfs.Fill(dataSetProfs, "Profesores");
+
+
+
+            // Cerramos la conexion
+            con.Close();
         }
     }
 }

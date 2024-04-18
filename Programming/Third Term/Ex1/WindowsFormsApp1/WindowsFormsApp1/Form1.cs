91ba497a38dc386413adbd433a5d497faf7d38c9
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2024-04-18 17:27:17 +0200

    Update Form1.cs

--- Programming/Third Term/Ex1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ Programming/Third Term/Ex1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -18,11 +18,245 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+        // Global Variables
         DataSet dataSetProfs;
         SqlDataAdapter dataAdapterProfs;
         private int pos;
         private int maxRegistros;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cadenaConexion = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\sanlor2\\Desktop\\Repos\\1Daw\\1daw\\Programming\\Third Term\\Ex1\\WindowsFormsApp1\\WindowsFormsApp1\\App_Data\\Instituto.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(cadenaConexion);
+            // Abrimos la conexión.
+            con.Open();
 
+            string cadenaSQL = "SELECT * From Profesores";
+            dataAdapterProfs = new SqlDataAdapter(cadenaSQL, con);
+            dataSetProfs = new DataSet();
+
+            dataAdapterProfs.Fill(dataSetProfs, "Profesores");
+            pos = 0;
+            maxRegistros = dataSetProfs.Tables["Profesores"].Rows.Count;
+            MostrarRegistro(pos);
+            btnSaveNew.Enabled = false;
+
+
+            // Cerramos la conexion
+            con.Close();
+        }
+        
+        // Buttons
+        private void btnShowFirstTeacher_Click(object sender, EventArgs e)
+        {
+            UpdateIfWasChangedAndIsValid();
+            pos = 0;
+            MostrarRegistro(pos);
+        }
+
+        private void btnShowNextTeacher_Click(object sender, EventArgs e)
+        {
+            UpdateIfWasChangedAndIsValid();
+            if (pos != maxRegistros - 1)
+            {
+                pos++;
+            }
+            else
+            {
+                pos = 0;
+            }
+            MostrarRegistro(pos);
+        }
+
+        private void btnShowPreviousTeacher_Click(object sender, EventArgs e)
+        {
+            UpdateIfWasChangedAndIsValid();
+            if (pos != 0)
+            {
+                pos--;
+            }
+            else
+            {
+                pos = maxRegistros - 1;
+            }
+            MostrarRegistro(pos);
+            
+        }
+
+        private void btnShowLastTeacher_Click(object sender, EventArgs e)
+        {
+            UpdateIfWasChangedAndIsValid();
+            pos = maxRegistros - 1;
+            MostrarRegistro(pos);
+        }
+
+        private void btnAddTeacher_Click(object sender, EventArgs e)
+        {
+            txtDNI.Clear();
+            txtNombre.Clear();
+            txtApellidos.Clear();
+            txtTlf.Clear();
+            txteMail.Clear();
+
+            btnSaveNew.Enabled = true;
+            btnAddTeacher.Enabled = false;
+        }
+
+        private void btnSaveNew_Click(object sender, EventArgs e)
+        {
+            // Validamos el nuevo registro
+            List<int> IsValid = EntryIsValid();
+
+            if (IsValid.Count > 0)
+            {
+                string errorMessage = "";
+                if (IsValid.Contains(-1))
+                {
+                    errorMessage += "*DNI\n";
+                }
+                if (IsValid.Contains(-2))
+                {
+                    errorMessage += "*Nombre\n";
+                }
+                if (IsValid.Contains(-3))
+                {
+                    errorMessage += "*Apellidos\n";
+                }
+                if (IsValid.Contains(-4))
+                {
+                    errorMessage += "*Telefono\n";
+                }
+                if (IsValid.Contains(-5))
+                {
+                    errorMessage += "*Email\n";
+                }
+                errorMessage = "Los siguientes parametros son erroneos: \n" + errorMessage;
+                MessageBox.Show(errorMessage);
+            }
+            else
+            {
+
+                // Creamos un nuevo registro.
+                DataRow dRegistro = dataSetProfs.Tables["Profesores"].NewRow();
+                // Metemos los datos en el nuevo registro
+                dRegistro[0] = txtDNI.Text;
+                dRegistro[1] = txtNombre.Text;
+                dRegistro[2] = txtApellidos.Text;
+                dRegistro[3] = txtTlf.Text;
+                dRegistro[4] = txteMail.Text;
+
+
+                // Añadimos el registro al Dataset
+                dataSetProfs.Tables["Profesores"].Rows.Add(dRegistro);
+                // Reconectamos con el dataAdapter y actualizamos la BD
+                SqlCommandBuilder cb = new SqlCommandBuilder(dataAdapterProfs);
+                dataAdapterProfs.Update(dataSetProfs, "Profesores");
+                // Actualizamos el número de registros y la posición en la tabla
+                maxRegistros++;
+                pos = maxRegistros - 1;
+                
+                MostrarRegistro(pos);
+                btnSaveNew.Enabled = false;
+
+            }
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateRow(pos);
+        }
+
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult wantToDelete;
+            wantToDelete = MessageBox.Show("Are you sure that you want to delete this teacher?", " ", MessageBoxButtons.YesNo);
+            if (wantToDelete == DialogResult.Yes)
+            {
+                // Eliminamos el registro situado en la posición actual.
+                dataSetProfs.Tables["Profesores"].Rows[pos].Delete();
+                // Tenemos un registro menos
+                maxRegistros--;
+                if (maxRegistros >= 1)
+                {
+                    // Nos vamos al primer registro y lo mostramos
+                    pos = 0;
+
+                    // Reconectamos con el dataAdapter y actualizamos la BD
+                    SqlCommandBuilder cb = new SqlCommandBuilder(dataAdapterProfs);
+                    dataAdapterProfs.Update(dataSetProfs, "Profesores");
+
+                    MostrarRegistro(pos);
+                    MessageBox.Show("Student Deleted");
+                }
+                else
+                {
+                    MostrarVacio();
+                }
+            }
+        }
+
+        private void btnLookBySurname_Click(object sender, EventArgs e)
+        {
+            string surname = Interaction.InputBox("What teacher do you want to look for?");
+            int index = GetIndexBySurname(surname);
+            if (index != -1)
+            {
+                MostrarRegistro(index);
+            } else
+            {
+                MessageBox.Show("That teacher is not in the database");
+            }
+        }
+
+
+        private void btnShowEveryTeacher_Click(object sender, EventArgs e)
+        {
+            string everyTeacher = GetEveryTeacher();
+            MessageBox.Show("The teachers are: \n" + everyTeacher);
+
+        }
+        // TextBoxes
+        private void txtDNI_TextChanged(object sender, EventArgs e)
+        {
+            if (EntryIsValid().Count == 0)
+            {
+                btnUpdate.Enabled = true;
+            }
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            if (EntryIsValid().Count == 0)
+            {
+                btnUpdate.Enabled = true;
+            }
+        }
+
+        private void txtTlf_TextChanged(object sender, EventArgs e)
+        {
+            if (EntryIsValid().Count == 0)
+            {
+                btnUpdate.Enabled = true;
+            }
+        }
+
+        private void txtApellidos_TextChanged(object sender, EventArgs e)
+        {
+            if (EntryIsValid().Count == 0)
+            {
+                btnUpdate.Enabled = true;
+            }
+        }
+
+        private void txteMail_TextChanged(object sender, EventArgs e)
+        {
+            if (EntryIsValid().Count == 0)
+            {
+                btnUpdate.Enabled = true;
+            }
+        }
+    
+        // Functions
         public bool checkIfChanges(int pos)
         {
             bool itChanged = false;
@@ -49,12 +283,24 @@ namespace WindowsFormsApp1
                 itChanged = true;
             }
             return itChanged;
-            /* dRegistro["DNI"] = txtDNI.Text;
-            dRegistro["Nombre"] = txtNombre.Text;
-            dRegistro["Apellido"] = txtApellidos.Text;
-            dRegistro["Tlf"] = txtTlf.Text;
-            dRegistro["EMail"] = txteMail.Text;*/
-
+        }
+        public void UpdateIfWasChangedAndIsValid()
+        {
+            if (EntryIsValid().Count == 0)
+            {
+                if (checkIfChanges(pos))
+                {
+                    DialogResult wantToUpdate = MessageBox.Show("Do you want to update this teacher data?", " ", MessageBoxButtons.YesNo);
+                    if (wantToUpdate == DialogResult.Yes)
+                    {
+                        UpdateRow(pos);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Changes were detected, but as they are not valid, data wasnt updated");
+            }
         }
 
         public string GetEveryTeacher()
@@ -70,7 +316,7 @@ namespace WindowsFormsApp1
             return teacherList;
 
         }
-    public int GetIndexBySurname(string surname)
+        public int GetIndexBySurname(string surname)
         {
             int rowIndex = -1;
             for (int i = 0; i <= maxRegistros - 1; i++)
@@ -161,27 +407,6 @@ namespace WindowsFormsApp1
             }
             return validationCodes;
         }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            string cadenaConexion = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\sanlor2\\Desktop\\Repos\\1Daw\\1daw\\Programming\\Third Term\\Ex1\\WindowsFormsApp1\\WindowsFormsApp1\\App_Data\\Instituto.mdf;Integrated Security=True";
-            SqlConnection con = new SqlConnection(cadenaConexion);
-            // Abrimos la conexión.
-            con.Open();
-
-            string cadenaSQL = "SELECT * From Profesores";
-            dataAdapterProfs = new SqlDataAdapter(cadenaSQL, con);
-            dataSetProfs = new DataSet();
-
-            dataAdapterProfs.Fill(dataSetProfs, "Profesores");
-            pos = 0;
-            maxRegistros = dataSetProfs.Tables["Profesores"].Rows.Count;
-            MostrarRegistro(pos);
-            btnSaveNew.Enabled = false;
-
-
-            // Cerramos la conexion
-            con.Close();
-        }
         public void MostrarRegistro(int pos)
         {
             // Objeto que nos permite recoger un registro de la tabla.
@@ -206,7 +431,7 @@ namespace WindowsFormsApp1
         public void MostrarVacio()
         {
             txtDNI.Text = string.Empty;
-            txtNombre.Text = string.Empty; 
+            txtNombre.Text = string.Empty;
             txtApellidos.Text = string.Empty;
             txtTlf.Text = string.Empty;
             txteMail.Text = string.Empty;
@@ -216,134 +441,7 @@ namespace WindowsFormsApp1
             CheckButtons();
 
         }
-
-        private void btnShowFirstTeacher_Click(object sender, EventArgs e)
-        {
-            if (checkIfChanges(pos))
-            {
-                DialogResult wantToUpdate = MessageBox.Show("Do you want to update this teacher data?"," " ,MessageBoxButtons.YesNo);
-                if (wantToUpdate == DialogResult.Yes)
-                {
-                    if (EntryIsValid().Count == 0) 
-                    {
-                        SqlCommandBuilder cb = new SqlCommandBuilder(dataAdapterProfs);
-                        dataAdapterProfs.Update(dataSetProfs, "Profesores");
-                    }
-                    else
-                    {
-                        MessageBox.Show("La entrada no era correcta. No se ha cambiando");
-                    }
-                }
-            }
-            pos = 0;
-            MostrarRegistro(pos);
-        }
-
-        private void btnShowNextTeacher_Click(object sender, EventArgs e)
-        {
-            if (pos != maxRegistros - 1)
-            {
-                pos++;
-            }
-            else
-            {
-                pos = 0;
-            }
-            MostrarRegistro(pos);
-        }
-
-        private void btnShowPreviousTeacher_Click(object sender, EventArgs e)
-        {
-            if (pos != 0)
-            {
-                pos--;
-            }
-            else
-            {
-                pos = maxRegistros - 1;
-            }
-            MostrarRegistro(pos);
-            
-        }
-
-        private void btnShowLastTeacher_Click(object sender, EventArgs e)
-        {
-            pos = maxRegistros - 1;
-            MostrarRegistro(pos);
-        }
-
-        private void btnAddTeacher_Click(object sender, EventArgs e)
-        {
-            txtDNI.Clear();
-            txtNombre.Clear();
-            txtApellidos.Clear();
-            txtTlf.Clear();
-            txteMail.Clear();
-
-            btnSaveNew.Enabled = true;
-            btnAddTeacher.Enabled = false;
-        }
-
-        private void btnSaveNew_Click(object sender, EventArgs e)
-        {
-            // Validamos el nuevo registro
-            List<int> IsValid = EntryIsValid();
-
-            if (IsValid.Count > 0)
-            {
-                string errorMessage = "";
-                if (IsValid.Contains(-1))
-                {
-                    errorMessage += "*DNI\n";
-                }
-                if (IsValid.Contains(-2))
-                {
-                    errorMessage += "*Nombre\n";
-                }
-                if (IsValid.Contains(-3))
-                {
-                    errorMessage += "*Apellidos\n";
-                }
-                if (IsValid.Contains(-4))
-                {
-                    errorMessage += "*Telefono\n";
-                }
-                if (IsValid.Contains(-5))
-                {
-                    errorMessage += "*Email\n";
-                }
-                errorMessage = "Los siguientes parametros son erroneos: \n" + errorMessage;
-                MessageBox.Show(errorMessage);
-            }
-            else
-            {
-
-                // Creamos un nuevo registro.
-                DataRow dRegistro = dataSetProfs.Tables["Profesores"].NewRow();
-                // Metemos los datos en el nuevo registro
-                dRegistro[0] = txtDNI.Text;
-                dRegistro[1] = txtNombre.Text;
-                dRegistro[2] = txtApellidos.Text;
-                dRegistro[3] = txtTlf.Text;
-                dRegistro[4] = txteMail.Text;
-
-
-                // Añadimos el registro al Dataset
-                dataSetProfs.Tables["Profesores"].Rows.Add(dRegistro);
-                // Reconectamos con el dataAdapter y actualizamos la BD
-                SqlCommandBuilder cb = new SqlCommandBuilder(dataAdapterProfs);
-                dataAdapterProfs.Update(dataSetProfs, "Profesores");
-                // Actualizamos el número de registros y la posición en la tabla
-                maxRegistros++;
-                pos = maxRegistros - 1;
-                
-                MostrarRegistro(pos);
-                btnSaveNew.Enabled = false;
-
-            }
-        }
-
-        private void btnUpdate_Click(object sender, EventArgs e)
+        public void UpdateRow(int pos)
         {
             // Cogemos el registro situado en la posición actual.
             DataRow dRegistro = dataSetProfs.Tables["Profesores"].Rows[pos];
@@ -354,101 +452,12 @@ namespace WindowsFormsApp1
             dRegistro[3] = txtTlf.Text;
             dRegistro[4] = txteMail.Text;
             // Si quisieramos hacerlo por nombre de columna en vez de posición
-            
+
             // Reconectamos con el dataAdapter y actualizamos la BD
             SqlCommandBuilder cb = new SqlCommandBuilder(dataAdapterProfs);
             dataAdapterProfs.Update(dataSetProfs, "Profesores");
         }
-
         
-
-        private void btnDelete_Click(object sender, EventArgs e)
-        {
-            DialogResult wantToDelete;
-            wantToDelete = MessageBox.Show("Are you sure that you want to delete this teacher?", " ", MessageBoxButtons.YesNo);
-            if (wantToDelete == DialogResult.Yes)
-            {
-                // Eliminamos el registro situado en la posición actual.
-                dataSetProfs.Tables["Profesores"].Rows[pos].Delete();
-                // Tenemos un registro menos
-                maxRegistros--;
-                if (maxRegistros >= 1)
-                {
-                    // Nos vamos al primer registro y lo mostramos
-                    pos = 0;
-
-                    // Reconectamos con el dataAdapter y actualizamos la BD
-                    SqlCommandBuilder cb = new SqlCommandBuilder(dataAdapterProfs);
-                    dataAdapterProfs.Update(dataSetProfs, "Profesores");
-
-                    MostrarRegistro(pos);
-                    MessageBox.Show("Student Deleted");
-                }
-                else
-                {
-                    MostrarVacio();
-                }
-            }
-        }
-
-        private void btnLookBySurname_Click(object sender, EventArgs e)
-        {
-            string surname = Interaction.InputBox("What teacher do you want to look for?");
-            int index = GetIndexBySurname(surname);
-            if (index != -1)
-            {
-                MostrarRegistro(index);
-            } else
-            {
-                MessageBox.Show("That teacher is not in the database");
-            }
-        }
-
-
-        private void btnShowEveryTeacher_Click(object sender, EventArgs e)
-        {
-            string everyTeacher = GetEveryTeacher();
-            MessageBox.Show("The teachers are: \n" + everyTeacher);
-
-        }
-        private void txtDNI_TextChanged(object sender, EventArgs e)
-        {
-            if (EntryIsValid().Count == 0)
-            {
-                btnUpdate.Enabled = true;
-            }
-        }
-
-        private void txtNombre_TextChanged(object sender, EventArgs e)
-        {
-            if (EntryIsValid().Count == 0)
-            {
-                btnUpdate.Enabled = true;
-            }
-        }
-
-        private void txtTlf_TextChanged(object sender, EventArgs e)
-        {
-            if (EntryIsValid().Count == 0)
-            {
-                btnUpdate.Enabled = true;
-            }
-        }
-
-        private void txtApellidos_TextChanged(object sender, EventArgs e)
-        {
-            if (EntryIsValid().Count == 0)
-            {
-                btnUpdate.Enabled = true;
-            }
-        }
-
-        private void txteMail_TextChanged(object sender, EventArgs e)
-        {
-            if (EntryIsValid().Count == 0)
-            {
-                btnUpdate.Enabled = true;
-            }
-        }
     }
+
 }

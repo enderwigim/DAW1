0beac199f108269c2bfecea2396503cd3cc3298e
SebastianLorenzano <118050694+SebastianLorenzano@users.noreply.github.com>
2024-04-29 10:53:53 +0200

    Update Driver.cs

--- LMSGI/Trash/WebDriver/Subscribing NewsLetter WebDriving/Subscribing NewsLetter WebDriving/Driver.cs
+++ LMSGI/Trash/WebDriver/Subscribing NewsLetter WebDriving/Subscribing NewsLetter WebDriving/Driver.cs
@@ -12,9 +12,9 @@ namespace Subscribing_NewsLetter_WebDriving
 {
     public class Driver
     {
-        private IWebDriver _chromeDriver;
-
-
+        private IWebDriver _chromeDriver;        // Seba: Inicia en nulo, en el constructor lo inicias, pero, te vas a acordar de hacerlo en todos los constructores?
+                                                    // si te olvidas, te explota la aplicación: Si no ganas nada inicializando el atributo en el constructor, no lo inicies ahi,
+                                                    // a parte consume espacio que en este constructor no importa, pero en algunos que ya tienen otras cosas ocupa espacio
         public Driver()
         {
             // Create an instance for Chrome.
@@ -31,15 +31,11 @@ namespace Subscribing_NewsLetter_WebDriving
         {
             try
             {
-                var buttonDriver = _chromeDriver.FindElement(By.XPath(button.XPath));
-                if (button.Type.ToLower() == "click")
-                {
+                var buttonDriver = _chromeDriver.FindElement(By.XPath(button.XPath));    // Seba: Estos if esta bien aqui ya que son lo suficientemente pequeños, si fuesen 
+                if (button.Type.ToLower() == "click")                                      // mas grandes siempre podes pasarlos a funciones.
                     buttonDriver.Click();
-                }
                 else if (button.Type.ToLower() == "text")
-                {
                     buttonDriver.SendKeys(button.Insert);
-                }
                 else if (button.Type.ToLower() == "select")
                 {
                     SelectElement selectButton = new SelectElement(buttonDriver);
@@ -52,7 +48,7 @@ namespace Subscribing_NewsLetter_WebDriving
                 Console.Error.WriteLine(ex.ToString());
                 return;
                 
-                
+                                        // Seba: Cuida los espacios, formatea bien el codigo. 
             }
         }
         public void DriveClose()

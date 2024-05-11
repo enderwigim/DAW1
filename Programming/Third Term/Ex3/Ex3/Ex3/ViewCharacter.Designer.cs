ba4cbc118873276a769e2394af4042402443292a
Santiago Lorenzano <enderwigim@gmail.com>
2024-05-11 14:46:16 +0200

    as

--- Programming/Third Term/Ex3/Ex3/Ex3/ViewCharacter.Designer.cs
+++ Programming/Third Term/Ex3/Ex3/Ex3/ViewCharacter.Designer.cs
@@ -47,9 +47,10 @@
             // btnShowNext
             // 
             this.btnShowNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
-            this.btnShowNext.Location = new System.Drawing.Point(325, 375);
+            this.btnShowNext.Location = new System.Drawing.Point(433, 462);
+            this.btnShowNext.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnShowNext.Name = "btnShowNext";
-            this.btnShowNext.Size = new System.Drawing.Size(112, 32);
+            this.btnShowNext.Size = new System.Drawing.Size(149, 39);
             this.btnShowNext.TabIndex = 63;
             this.btnShowNext.Text = "NEXT";
             this.btnShowNext.UseVisualStyleBackColor = true;
@@ -58,9 +59,10 @@
             // btnShowPrevious
             // 
             this.btnShowPrevious.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
-            this.btnShowPrevious.Location = new System.Drawing.Point(66, 375);
+            this.btnShowPrevious.Location = new System.Drawing.Point(88, 462);
+            this.btnShowPrevious.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnShowPrevious.Name = "btnShowPrevious";
-            this.btnShowPrevious.Size = new System.Drawing.Size(112, 32);
+            this.btnShowPrevious.Size = new System.Drawing.Size(149, 39);
             this.btnShowPrevious.TabIndex = 62;
             this.btnShowPrevious.Text = "PREVIOUS";
             this.btnShowPrevious.UseVisualStyleBackColor = true;
@@ -70,9 +72,10 @@
             // 
             this.lblLocation.AutoSize = true;
             this.lblLocation.Font = new System.Drawing.Font("Sylfaen", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLocation.Location = new System.Drawing.Point(91, 294);
+            this.lblLocation.Location = new System.Drawing.Point(121, 362);
+            this.lblLocation.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLocation.Name = "lblLocation";
-            this.lblLocation.Size = new System.Drawing.Size(111, 25);
+            this.lblLocation.Size = new System.Drawing.Size(135, 31);
             this.lblLocation.TabIndex = 61;
             this.lblLocation.Text = "Stormwind";
             // 
@@ -80,9 +83,10 @@
             // 
             this.lblLevel.AutoSize = true;
             this.lblLevel.Font = new System.Drawing.Font("Sylfaen", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLevel.Location = new System.Drawing.Point(158, 225);
+            this.lblLevel.Location = new System.Drawing.Point(211, 277);
+            this.lblLevel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLevel.Name = "lblLevel";
-            this.lblLevel.Size = new System.Drawing.Size(34, 25);
+            this.lblLevel.Size = new System.Drawing.Size(40, 31);
             this.lblLevel.TabIndex = 60;
             this.lblLevel.Text = "80";
             // 
@@ -90,9 +94,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Sylfaen", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(91, 225);
+            this.label4.Location = new System.Drawing.Point(121, 277);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 25);
+            this.label4.Size = new System.Drawing.Size(79, 31);
             this.label4.TabIndex = 59;
             this.label4.Text = "Level:";
             // 
@@ -100,9 +105,10 @@
             // 
             this.lblFaction.AutoSize = true;
             this.lblFaction.Font = new System.Drawing.Font("Sylfaen", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFaction.Location = new System.Drawing.Point(91, 170);
+            this.lblFaction.Location = new System.Drawing.Point(121, 209);
+            this.lblFaction.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFaction.Name = "lblFaction";
-            this.lblFaction.Size = new System.Drawing.Size(87, 25);
+            this.lblFaction.Size = new System.Drawing.Size(103, 31);
             this.lblFaction.TabIndex = 57;
             this.lblFaction.Text = "Alliance";
             // 
@@ -110,18 +116,20 @@
             // 
             this.lblClass.AutoSize = true;
             this.lblClass.Font = new System.Drawing.Font("Sylfaen", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblClass.Location = new System.Drawing.Point(91, 108);
+            this.lblClass.Location = new System.Drawing.Point(121, 133);
+            this.lblClass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblClass.Name = "lblClass";
-            this.lblClass.Size = new System.Drawing.Size(79, 25);
+            this.lblClass.Size = new System.Drawing.Size(95, 31);
             this.lblClass.TabIndex = 56;
             this.lblClass.Text = "Paladin";
             // 
             // characterImg
             // 
             this.characterImg.Image = ((System.Drawing.Image)(resources.GetObject("characterImg.Image")));
-            this.characterImg.Location = new System.Drawing.Point(250, 108);
+            this.characterImg.Location = new System.Drawing.Point(333, 133);
+            this.characterImg.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.characterImg.Name = "characterImg";
-            this.characterImg.Size = new System.Drawing.Size(158, 251);
+            this.characterImg.Size = new System.Drawing.Size(211, 309);
             this.characterImg.TabIndex = 55;
             this.characterImg.TabStop = false;
             // 
@@ -129,18 +137,20 @@
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Sylfaen", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(191, 32);
+            this.lblName.Location = new System.Drawing.Point(255, 39);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(86, 35);
+            this.lblName.Size = new System.Drawing.Size(110, 44);
             this.lblName.TabIndex = 54;
             this.lblName.Text = "Ender";
             // 
             // btnChangeCharacters
             // 
             this.btnChangeCharacters.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
-            this.btnChangeCharacters.Location = new System.Drawing.Point(195, 375);
+            this.btnChangeCharacters.Location = new System.Drawing.Point(260, 462);
+            this.btnChangeCharacters.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnChangeCharacters.Name = "btnChangeCharacters";
-            this.btnChangeCharacters.Size = new System.Drawing.Size(112, 32);
+            this.btnChangeCharacters.Size = new System.Drawing.Size(149, 39);
             this.btnChangeCharacters.TabIndex = 64;
             this.btnChangeCharacters.Text = "CHANGE";
             this.btnChangeCharacters.UseVisualStyleBackColor = true;
@@ -149,17 +159,18 @@
             // factionImg
             // 
             this.factionImg.Image = ((System.Drawing.Image)(resources.GetObject("factionImg.Image")));
-            this.factionImg.Location = new System.Drawing.Point(325, 18);
+            this.factionImg.Location = new System.Drawing.Point(433, 22);
+            this.factionImg.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.factionImg.Name = "factionImg";
-            this.factionImg.Size = new System.Drawing.Size(82, 84);
+            this.factionImg.Size = new System.Drawing.Size(109, 103);
             this.factionImg.TabIndex = 65;
             this.factionImg.TabStop = false;
             // 
             // SelectCharacter
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(492, 443);
+            this.ClientSize = new System.Drawing.Size(656, 545);
             this.Controls.Add(this.factionImg);
             this.Controls.Add(this.btnChangeCharacters);
             this.Controls.Add(this.btnShowNext);
@@ -171,6 +182,7 @@
             this.Controls.Add(this.lblClass);
             this.Controls.Add(this.characterImg);
             this.Controls.Add(this.lblName);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SelectCharacter";
             this.Text = "SelectCharacter";
             this.Load += new System.EventHandler(this.ViewCharacter_Load);

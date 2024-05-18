5434aeb0b70d1c3b38985534ef2115335d61f46a
Santiago Lorenzano <enderwigim@gmail.com>
2024-05-18 22:20:19 +0200

    Update Program.cs

--- LMSGI/Tests/SerializingTest/SerializingTest/Program.cs
+++ LMSGI/Tests/SerializingTest/SerializingTest/Program.cs
@@ -12,14 +12,35 @@ namespace SerializingTest
     {
         static void Main(string[] args)
         { 
+            // Create pageList
+            PageList pageList = new PageList();
+            //Create page
+            Page pageToAdd = new Page();
+            pageToAdd.Url = "https://discord.com/";
+            pageToAdd.Buttons = new List<Button>();
+
+            //Create Button
+            Button button = new Button();
+            button.Name = "login";
+            button.XPath = "/html/body/div[1]/div[1]/div/div[1]/div[1]/a";
+            button.Type = "click";
+            
+            // Add Button into Page
+            pageToAdd.Buttons.Add(button);
+
+            // Add pageToAdd into pageList
+            pageList.Pages.Add(pageToAdd);
+
+
+            
+        }
+        public PageList DeserializeXMLInToPageList()
+        {
             var serializer = new XmlSerializer(typeof(PageList));
             using (StreamReader reader = new StreamReader("webs.xml"))
             {
                 var pageList = (PageList)serializer.Deserialize(reader);
-                Console.WriteLine(pageList);
-                
-                
-                Console.ReadLine();
+                return pageList;
             }
         }
     }

6356ef4b1b6dbfeb996bdcadffdf67192f2ddd42
Santiago Lorenzano <111286211+enderwigim@users.noreply.github.com>
2024-05-21 18:53:08 +0200

    Ex5 started

---
+++ Programming/Third Term/Ex5/Ex5/Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace Ex5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnPreviousClass = new System.Windows.Forms.Button();
+            this.btnNextClass = new System.Windows.Forms.Button();
+            this.lblClassSelector = new System.Windows.Forms.Label();
+            this.characterImg = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnShowFirst = new System.Windows.Forms.Button();
+            this.btnShowNext = new System.Windows.Forms.Button();
+            this.btnShowPrevious = new System.Windows.Forms.Button();
+            this.btnShowLast = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAddNewCharacter = new System.Windows.Forms.Button();
+            this.btnCreateNew = new System.Windows.Forms.Button();
+            this.btnDeleteChar = new System.Windows.Forms.Button();
+            this.btnUpdateChar = new System.Windows.Forms.Button();
+            this.lblEntryNumber = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.txtLevel = new System.Windows.Forms.TextBox();
+            this.txtFaction = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.charCrud = new System.Windows.Forms.BindingSource(this.components);
+            this.characterDataSet = new Ex5.characterDataSet();
+            this.characterTableAdapter = new Ex5.characterDataSetTableAdapters.characterTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.characterImg)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.charCrud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.characterDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPreviousClass
+            // 
+            this.btnPreviousClass.Location = new System.Drawing.Point(519, 410);
+            this.btnPreviousClass.Name = "btnPreviousClass";
+            this.btnPreviousClass.Size = new System.Drawing.Size(89, 29);
+            this.btnPreviousClass.TabIndex = 54;
+            this.btnPreviousClass.Text = "Previous";
+            this.btnPreviousClass.UseVisualStyleBackColor = true;
+            // 
+            // btnNextClass
+            // 
+            this.btnNextClass.Location = new System.Drawing.Point(688, 410);
+            this.btnNextClass.Name = "btnNextClass";
+            this.btnNextClass.Size = new System.Drawing.Size(89, 29);
+            this.btnNextClass.TabIndex = 47;
+            this.btnNextClass.Text = "Next";
+            this.btnNextClass.UseVisualStyleBackColor = true;
+            // 
+            // lblClassSelector
+            // 
+            this.lblClassSelector.AutoSize = true;
+            this.lblClassSelector.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClassSelector.Location = new System.Drawing.Point(616, 375);
+            this.lblClassSelector.Name = "lblClassSelector";
+            this.lblClassSelector.Size = new System.Drawing.Size(55, 24);
+            this.lblClassSelector.TabIndex = 53;
+            this.lblClassSelector.Text = "Class";
+            // 
+            // characterImg
+            // 
+            this.characterImg.Location = new System.Drawing.Point(553, 103);
+            this.characterImg.Name = "characterImg";
+            this.characterImg.Size = new System.Drawing.Size(178, 269);
+            this.characterImg.TabIndex = 52;
+            this.characterImg.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnShowFirst);
+            this.groupBox2.Controls.Add(this.btnShowNext);
+            this.groupBox2.Controls.Add(this.btnShowPrevious);
+            this.groupBox2.Controls.Add(this.btnShowLast);
+            this.groupBox2.Location = new System.Drawing.Point(251, 177);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(187, 237);
+            this.groupBox2.TabIndex = 51;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Nav";
+            // 
+            // btnShowFirst
+            // 
+            this.btnShowFirst.Location = new System.Drawing.Point(5, 18);
+            this.btnShowFirst.Name = "btnShowFirst";
+            this.btnShowFirst.Size = new System.Drawing.Size(168, 46);
+            this.btnShowFirst.TabIndex = 14;
+            this.btnShowFirst.Text = "First Character";
+            this.btnShowFirst.UseVisualStyleBackColor = true;
+            this.btnShowFirst.Click += new System.EventHandler(this.btnShowFirst_Click);
+            // 
+            // btnShowNext
+            // 
+            this.btnShowNext.Location = new System.Drawing.Point(5, 71);
+            this.btnShowNext.Name = "btnShowNext";
+            this.btnShowNext.Size = new System.Drawing.Size(168, 46);
+            this.btnShowNext.TabIndex = 15;
+            this.btnShowNext.Text = "Next";
+            this.btnShowNext.UseVisualStyleBackColor = true;
+            this.btnShowNext.Click += new System.EventHandler(this.btnShowNext_Click);
+            // 
+            // btnShowPrevious
+            // 
+            this.btnShowPrevious.Location = new System.Drawing.Point(5, 124);
+            this.btnShowPrevious.Name = "btnShowPrevious";
+            this.btnShowPrevious.Size = new System.Drawing.Size(168, 46);
+            this.btnShowPrevious.TabIndex = 16;
+            this.btnShowPrevious.Text = "Previous";
+            this.btnShowPrevious.UseVisualStyleBackColor = true;
+            this.btnShowPrevious.Click += new System.EventHandler(this.btnShowPrevious_Click);
+            // 
+            // btnShowLast
+            // 
+            this.btnShowLast.Location = new System.Drawing.Point(5, 176);
+            this.btnShowLast.Name = "btnShowLast";
+            this.btnShowLast.Size = new System.Drawing.Size(168, 46);
+            this.btnShowLast.TabIndex = 17;
+            this.btnShowLast.Text = "Last";
+            this.btnShowLast.UseVisualStyleBackColor = true;
+            this.btnShowLast.Click += new System.EventHandler(this.btnShowLast_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAddNewCharacter);
+            this.groupBox1.Controls.Add(this.btnCreateNew);
+            this.groupBox1.Controls.Add(this.btnDeleteChar);
+            this.groupBox1.Controls.Add(this.btnUpdateChar);
+            this.groupBox1.Location = new System.Drawing.Point(46, 177);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(187, 237);
+            this.groupBox1.TabIndex = 50;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CRUD";
+            // 
+            // btnAddNewCharacter
+            // 
+            this.btnAddNewCharacter.Location = new System.Drawing.Point(5, 18);
+            this.btnAddNewCharacter.Name = "btnAddNewCharacter";
+            this.btnAddNewCharacter.Size = new System.Drawing.Size(168, 46);
+            this.btnAddNewCharacter.TabIndex = 13;
+            this.btnAddNewCharacter.Text = "Add New Character";
+            this.btnAddNewCharacter.UseVisualStyleBackColor = true;
+            // 
+            // btnCreateNew
+            // 
+            this.btnCreateNew.Location = new System.Drawing.Point(5, 71);
+            this.btnCreateNew.Name = "btnCreateNew";
+            this.btnCreateNew.Size = new System.Drawing.Size(168, 46);
+            this.btnCreateNew.TabIndex = 10;
+            this.btnCreateNew.Text = "Create New Character";
+            this.btnCreateNew.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteChar
+            // 
+            this.btnDeleteChar.Location = new System.Drawing.Point(5, 124);
+            this.btnDeleteChar.Name = "btnDeleteChar";
+            this.btnDeleteChar.Size = new System.Drawing.Size(168, 46);
+            this.btnDeleteChar.TabIndex = 11;
+            this.btnDeleteChar.Text = "Delete Character";
+            this.btnDeleteChar.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateChar
+            // 
+            this.btnUpdateChar.Location = new System.Drawing.Point(5, 176);
+            this.btnUpdateChar.Name = "btnUpdateChar";
+            this.btnUpdateChar.Size = new System.Drawing.Size(168, 46);
+            this.btnUpdateChar.TabIndex = 11;
+            this.btnUpdateChar.Text = "Update Character";
+            this.btnUpdateChar.UseVisualStyleBackColor = true;
+            // 
+            // lblEntryNumber
+            // 
+            this.lblEntryNumber.AutoSize = true;
+            this.lblEntryNumber.Location = new System.Drawing.Point(685, 17);
+            this.lblEntryNumber.Name = "lblEntryNumber";
+            this.lblEntryNumber.Size = new System.Drawing.Size(0, 13);
+            this.lblEntryNumber.TabIndex = 49;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(639, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 48;
+            this.label6.Text = "Registro: ";
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.charCrud, "location", true));
+            this.txtLocation.Location = new System.Drawing.Point(468, 62);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(309, 20);
+            this.txtLocation.TabIndex = 46;
+            // 
+            // txtLevel
+            // 
+            this.txtLevel.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.charCrud, "level", true));
+            this.txtLevel.Location = new System.Drawing.Point(65, 62);
+            this.txtLevel.Name = "txtLevel";
+            this.txtLevel.Size = new System.Drawing.Size(309, 20);
+            this.txtLevel.TabIndex = 45;
+            // 
+            // txtFaction
+            // 
+            this.txtFaction.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.charCrud, "faction", true));
+            this.txtFaction.Location = new System.Drawing.Point(65, 108);
+            this.txtFaction.Name = "txtFaction";
+            this.txtFaction.Size = new System.Drawing.Size(309, 20);
+            this.txtFaction.TabIndex = 44;
+            // 
+            // txtName
+            // 
+            this.txtName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.charCrud, "name", true));
+            this.txtName.Location = new System.Drawing.Point(65, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(309, 20);
+            this.txtName.TabIndex = 43;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 42;
+            this.label5.Text = "Faction";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(408, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "Location";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 40;
+            this.label3.Text = "Level";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Name";
+            // 
+            // charCrud
+            // 
+            this.charCrud.DataMember = "character";
+            this.charCrud.DataSource = this.characterDataSet;
+            // 
+            // characterDataSet
+            // 
+            this.characterDataSet.DataSetName = "characterDataSet";
+            this.characterDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // characterTableAdapter
+            // 
+            this.characterTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 473);
+            this.Controls.Add(this.btnPreviousClass);
+            this.Controls.Add(this.btnNextClass);
+            this.Controls.Add(this.lblClassSelector);
+            this.Controls.Add(this.characterImg);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblEntryNumber);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.txtLevel);
+            this.Controls.Add(this.txtFaction);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.characterImg)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.charCrud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.characterDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPreviousClass;
+        private System.Windows.Forms.Button btnNextClass;
+        private System.Windows.Forms.Label lblClassSelector;
+        private System.Windows.Forms.PictureBox characterImg;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnShowFirst;
+        private System.Windows.Forms.Button btnShowNext;
+        private System.Windows.Forms.Button btnShowPrevious;
+        private System.Windows.Forms.Button btnShowLast;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAddNewCharacter;
+        private System.Windows.Forms.Button btnCreateNew;
+        private System.Windows.Forms.Button btnDeleteChar;
+        private System.Windows.Forms.Button btnUpdateChar;
+        private System.Windows.Forms.Label lblEntryNumber;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.TextBox txtLevel;
+        private System.Windows.Forms.TextBox txtFaction;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.BindingSource charCrud;
+        private characterDataSet characterDataSet;
+        private characterDataSetTableAdapters.characterTableAdapter characterTableAdapter;
+    }
+}
+
---
+++ Programming/Third Term/Ex5/Ex5/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'characterDataSet.character' Puede moverla o quitarla según sea necesario.
+            this.characterTableAdapter.Fill(this.characterDataSet.character);
+            ManageEntryLbl();
+
+        }
+
+        private void btnShowNext_Click(object sender, EventArgs e)
+        {
+            if (CustomRegex.RegexLevel(txtLevel.Text))
+            {
+                try
+                {
+                    charCrud.MoveNext();
+                    ManageEntryLbl();
+                }
+                catch (ConstraintException ex)
+                {
+                    charCrud.ResetItem(0);
+                    MessageBox.Show("An error ocurred while updating data: \n" + ex.Message);
+                    
+                }
+            } 
+            else
+            {
+                MessageBox.Show("That level is not in the correct format. It must be between 1 - 80");
+            }
+        }
+
+        private void btnShowFirst_Click(object sender, EventArgs e)
+        {
+            if (CustomRegex.RegexLevel(txtLevel.Text))
+            {
+                try
+                {
+                    charCrud.MoveFirst();
+                    ManageEntryLbl();
+                }
+                catch (ConstraintException ex)
+                {
+                    MessageBox.Show("An error ocurred while updating data: \n" + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("That level is not in the correct format. It must be between 1 - 80");
+            }
+        }
+        private void btnShowPrevious_Click(object sender, EventArgs e)
+        {
+            if (CustomRegex.RegexLevel(txtLevel.Text))
+            {
+                try
+                {
+                    charCrud.MovePrevious();
+                    ManageEntryLbl();
+                }
+                catch (ConstraintException ex)
+                {
+                    MessageBox.Show("An error ocurred while updating data: \n" + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("That level is not in the correct format. It must be between 1 - 80");
+            }
+        }
+
+        private void btnShowLast_Click(object sender, EventArgs e)
+        {
+            if (CustomRegex.RegexLevel(txtLevel.Text))
+            {
+                try
+                {
+                    charCrud.MoveLast();
+                    ManageEntryLbl();
+                }
+                catch (ConstraintException ex)
+                {
+                    MessageBox.Show("An error ocurred while updating data: \n" + ex.Message);
+                };
+            }
+            else
+            {
+                MessageBox.Show("That level is not in the correct format. It must be between 1 - 80");
+            }
+        }
+        public void ManageEntryLbl()
+        {
+            lblEntryNumber.Text = (charCrud.Position + 1) + " de " + charCrud.Count;
+        }
+        
+    }
+}

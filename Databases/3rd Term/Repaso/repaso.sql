4789ab1822658a6055f55233341b01f2ddfe6fa0
Santiago Lorenzano <enderwigim@gmail.com>
2024-05-23 19:01:15 +0200

    Update

--- Databases/3rd Term/Repaso/repaso.sql
+++ Databases/3rd Term/Repaso/repaso.sql
@@ -142,8 +142,127 @@ SELECT dbo.GetImporteTotalVendido(87)
 
    Crea un cursor que itere por la tabla PRODUCTOS e indique la siguiente información:
 	El producto XXX con referencia YYY aparece ZZZ veces en la tabla DETALLE_PEDIDOS. */
+USE JARDINERIA
+DECLARE @codEmpleado INT
+DECLARE @nombre VARCHAR(100)
+
+DECLARE Cur_Empleados CURSOR FOR
+SELECT codEmpleado,
+       nombre
+  FROM EMPLEADOS
+
+OPEN Cur_Empleados
+
+FETCH NEXT FROM Cur_Empleados INTO @codEmpleado, @nombre
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    DECLARE @cantEmpleados INT = 0
+    SELECT @cantEmpleados = COUNT(1)
+      FROM CLIENTES
+     WHERE codEmpl_ventas = @codEmpleado
+
+    PRINT CONCAT('ID: ', @codEmpleado, CHAR(10),
+                 'Nombre: ', @nombre, CHAR(10),
+                 'Cantidad de Clientes: ', @cantEmpleados, CHAR(10))
+    
+    FETCH NEXT FROM Cur_Empleados INTO @codEmpleado, @nombre
+END
+
+CLOSE Cur_Empleados
+DEALLOCATE Cur_Empleados
+
+SELECT COUNT(codCliente)
+  FROM CLIENTES
+ WHERE codEmpl_ventas = 17
+
+/*
+ Crea un cursor que itere por la tabla PRODUCTOS e indique la siguiente información:
+	El producto XXX con referencia YYY aparece ZZZ veces en la tabla DETALLE_PEDIDOS. */
+EXEC sp_help PRODUCTOS
+USE JARDINERIA
+GO
+DECLARE @nombre VARCHAR(100)
+DECLARE @codProducto INT
+DECLARE @apariciones INT
+
+DECLARE Cur_Productos CURSOR FOR
+SELECT codProducto,
+       nombre
+  FROM PRODUCTOS
+
+OPEN Cur_Productos
+
+FETCH NEXT FROM Cur_Productos INTO @codProducto, @nombre
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    SELECT @apariciones = ISNULL(COUNT(codProducto), 0)
+      FROM DETALLE_PEDIDOS
+     WHERE codProducto = @codProducto
+
+     PRINT CONCAT('El producto ', @nombre ,' con referencia ',
+                  @codProducto,' aparece ', @apariciones ,
+                  ' veces en la tabla DETALLE_PEDIDOS.', CHAR(10))
+
+    FETCH NEXT FROM Cur_Productos INTO @codProducto, @nombre
+END
+
+CLOSE Cur_Productos
+DEALLOCATE Cur_Productos
+/*
+D) Crea un trigger que se active cuando se inserte un nuevo cliente y que en caso 
+	de no tener un limite_credito > 10000 se impida su inserción.
+
+   Crea un trigger que haga una copia de seguridad de la tabla FORMA_PAGO en la tabla
+	HIST_FORMA_PAGO cuando se actualice o borre algún registro de esta tabla.
+	La tabla HIST_FORMA_PAGO tendrá además la fecha de operación que corresponderá
+	con la fecha en la que se ejecute el trigger. */
+USE JARDINERIA
+GO
+CREATE OR ALTER TRIGGER TX_CLIENTE ON CLIENTES
+INSTEAD OF INSERT
+AS
+BEGIN
+    BEGIN TRY
+        -- Declaramos una variable @openTran
+        DECLARE @openTran BIT = 1
+        -- Si no hay transacciones, comenzaremos una y setearemos nuestra variable a 0
+        IF @@TRANCOUNT = 0
+        BEGIN
+            SET @openTran = 0
+            BEGIN TRAN
+        END
+        -- Insertaremos solamente los clientes que tengan un limite_credito > 10000
+        INSERT INTO CLIENTES
+        SELECT INSERTED.*
+        FROM INSERTED
+        WHERE limite_credito > 10000;
+
+        IF @openTran = 0
+            COMMIT
+    END TRY
+    BEGIN CATCH
+        PRINT CONCAT('ERROR: ', ERROR_NUMBER(), CHAR(10),
+                     'LINE: ', ERROR_LINE(), CHAR(10),
+                     'PROCEDURE: ', ERROR_PROCEDURE(), CHAR(10),
+                     'NUMBER: ', ERROR_NUMBER(), CHAR(10))
+        IF @openTran = 0
+            ROLLBACK
+    END CATCH
+END
+GO
 
 
+DECLARE @nuevoCliente INT;
+SET @nuevoCliente = ISNULL((SELECT MAX(codCliente)
+                              FROM CLIENTES), 0) + 1
+
+INSERT INTO CLIENTES
+VALUES (@nuevoCliente, 'Santi', 'Pls', 'Copion', '696969696', null, 'Argentina', null, 'Alicante', null, null, null, 15000),
+       (@nuevoCliente + 1, 'Pedro', 'Gómez', 'López', '666666666', null, 'España', null, 'Madrid', null, null, null, 8000),
+       (@nuevoCliente + 2, 'María', 'Fernández', 'García', '555555555', null, 'España', null, 'Barcelona', null, null, null, 12000),
+       (@nuevoCliente + 3, 'Laura', 'Martínez', 'Rodríguez', '777777777', null, 'España', null, 'Valencia', null, null, null, 9500);
 
 /*
 D) Crea un trigger que se active cuando se inserte un nuevo cliente y que en caso 
@@ -153,3 +272,24 @@ D) Crea un trigger que se active cuando se inserte un nuevo cliente y que en cas
 	HIST_FORMA_PAGO cuando se actualice o borre algún registro de esta tabla.
 	La tabla HIST_FORMA_PAGO tendrá además la fecha de operación que corresponderá
 	con la fecha en la que se ejecute el trigger. */
+GO 
+
+SELECT *
+  INTO HIST_FORMA_PAGO
+  FROM FORMA_PAGO
+ WHERE 1 = 0
+
+ALTER TABLE HIST_FORMA_PAGO
+  ADD fecha_operacion DATE
+GO
+GO
+CREATE OR ALTER TRIGGER TX_FORMA_PAGO ON FORMA_PAGO
+AFTER UPDATE, DELETE
+AS
+BEGIN
+    -- Agregamos lo cambiado en al historico
+    INSERT INTO HIST_FORMA_PAGO
+    SELECT *, GETDATE()
+      FROM DELETED
+END
+GO
--- Databases/3rd Term/Repaso/triggers.sql
+++ Databases/3rd Term/Repaso/triggers.sql
@@ -146,7 +146,161 @@ END
   DELETE FROM SOCIOS
     WHERE DNI = '9876543210'
        OR DNI = '1234567890'
-
+GO
 USE master
 GO
 DROP DATABASE BIBLIOTECA2
+
+
+GO
+/*
+Disparador SERVICIO TECNICO
+Crear un disparador para que cuando demos de baja un Técnico se compruebe si la suma
+del importe de las reparaciones de este técnico es mayor que 2500 €, y si lo cumple,
+guardaremos sus datos personales en una tabla llamada TECNICOS_RESERVA con la
+misma estructura que la tabla TECNICOS más un campo FechaBaja (con la fecha en la que
+se realiza la baja, que será la fecha del sistema).
+TABLAS NECESARIAS
+TECNICOS (DNI, nombre, ciudad, salario)
+PK: DNI
+REPARACIONES (idReparacion, fecha, DNI_tecnico, concepto, importe)
+PK: idReparacion
+FK: DNI_tecnico → TECNICOS
+*/
+USE MASTER
+CREATE DATABASE SERVICIOTECNICO
+GO
+USE SERVICIOTECNICO
+GO
+CREATE TABLE TECNICOS(
+    DNI CHAR(10),
+    nombre VARCHAR(100) NOT NULL,
+    ciudad VARCHAR(100) NOT NULL,
+    salario DECIMAL(7,2) NOT NULL,
+    CONSTRAINT PK_TECNICOS PRIMARY KEY (DNI)
+)
+GO
+CREATE TABLE REPARACIONES(
+    idReparacion INT,
+    fecha DATE NOT NULL,
+    DNI_tecnico CHAR(10),
+    concepto VARCHAR(200),
+    importe DECIMAL(7,2) NOT NULL,
+    CONSTRAINT PK_REPARACIONES PRIMARY KEY (idReparacion),
+    CONSTRAINT FK_REPARACIONES_TECNICOS FOREIGN KEY (DNI_tecnico)
+    REFERENCES TECNICOS(DNI)
+)
+-- Creamos la tabla tecnicos reserva
+SELECT *
+  INTO TECNICOS_RESERVA
+  FROM TECNICOS
+
+ALTER TABLE TECNICOS_RESERVA
+  ADD fecha_baja DATE
+
+-- Insertamos los siguientes datos:
+INSERT INTO TECNICOS (DNI, nombre, ciudad, salario) 
+VALUES 
+('87654321X', 'Juan Pérez', 'Madrid', 2500.00),
+('12345678Z', 'María García', 'Barcelona', 2700.00),
+('98765432Y', 'Pedro Rodríguez', 'Valencia', 2600.00);
+
+-- Insertar datos de las reparaciones para el técnico Juan Pérez
+INSERT INTO REPARACIONES (idReparacion, fecha, DNI_tecnico, concepto, importe) 
+VALUES 
+(1, '2024-05-01 08:00:00', '87654321X', 'Reparación de ordenador portátil', 120.00),
+(2, '2024-05-02 10:30:00', '87654321X', 'Reparación de impresora', 80.00),
+(3, '2024-05-03 13:45:00', '87654321X', 'Reparación de teléfono móvil', 90.00);
+
+-- Insertar datos de las reparaciones para la técnica María García
+INSERT INTO REPARACIONES (idReparacion, fecha, DNI_tecnico, concepto, importe) 
+VALUES 
+(4, '2024-05-01 09:15:00', '12345678Z', 'Reparación de televisión', 200.00),
+(5, '2024-05-02 11:00:00', '12345678Z', 'Reparación de lavadora', 15000.00),
+(6, '2024-05-03 14:30:00', '12345678Z', 'Reparación de frigorífico', 180.00);
+
+-- Insertar datos de las reparaciones para el técnico Pedro Rodríguez
+INSERT INTO REPARACIONES (idReparacion, fecha, DNI_tecnico, concepto, importe) 
+VALUES 
+(7, '2024-05-01 10:00:00', '98765432Y', 'Reparación de aire acondicionado', 300.00),
+(8, '2024-05-02 12:00:00', '98765432Y', 'Reparación de calentador', 250.00),
+(9, '2024-05-03 15:00:00', '98765432Y', 'Reparación de secadora', 18000.00);
+
+
+/*
+Crear un disparador para que cuando demos de baja un Técnico se compruebe si la suma
+del importe de las reparaciones de este técnico es mayor que 2500 €, y si lo cumple,
+guardaremos sus datos personales en una tabla llamada TECNICOS_RESERVA con la
+misma estructura que la tabla TECNICOS más un campo FechaBaja (con la fecha en la que
+se realiza la baja, que será la fecha del sistema).
+*/
+GO
+CREATE OR ALTER TRIGGER TX_TECNICOS ON TECNICOS
+INSTEAD OF DELETE
+AS
+BEGIN
+    BEGIN TRY
+        DECLARE @openTran BIT = 1;
+        IF @@TRANCOUNT = 0
+            SET @openTran = 0
+
+        IF @openTran = 0
+            BEGIN TRAN
+        -- Seleccionamos los Datos de los tecnicos que hayan realizado al menos
+        -- una sumatoria de importes mayor a 2500
+        INSERT INTO TECNICOS_RESERVA(DNI, nombre, ciudad, salario, fecha_baja)
+        SELECT DELETED.*, 
+               GETDATE()
+          FROM DELETED
+         WHERE DNI IN (SELECT DNI_tecnico
+                           FROM REPARACIONES
+                          GROUP BY DNI_tecnico
+                          HAVING SUM(importe) >= 2500)
+
+        -- Realizamos un borrado en casacada
+        DELETE FROM REPARACIONES
+         WHERE DNI_tecnico IN (SELECT DNI
+                                 FROM DELETED);
+
+        DELETE FROM TECNICOS
+         WHERE DNI IN (SELECT DNI
+                         FROM DELETED);
+        IF @openTran = 0
+            COMMIT
+        
+    END TRY
+    BEGIN CATCH
+        PRINT CONCAT('ERROR: ', ERROR_MESSAGE(),
+                     'LINE: ', ERROR_LINE(),
+                     'PROCEDURE: ', ERROR_PROCEDURE(),
+                     'NUMBER: ', ERROR_NUMBER())
+        IF @openTran = 0
+           ROLLBACK
+
+    END CATCH
+END
+
+
+-- Validación
+BEGIN TRY
+    BEGIN TRAN
+    DELETE FROM TECNICOS
+     WHERE DNI = '12345678Z'
+       AND DNI = '87654321X'
+    COMMIT
+END TRY
+BEGIN CATCH
+    ROLLBACK
+END CATCH
+
+SELECT *
+  FROM TECNICOS
+
+GO
+USE master
+DROP DATABASE SERVICIOTECNICO
+
+
+/*
+
+*/
